827c6590f635f67d53198e47728e104c9e67457b
Julio Litzenberg <jltzbrg@gmail.com>
2022-09-07 16:20:46 +0300

    Adding react and typescript

---
+++ src/test.js
@@ -0,0 +1,4 @@
+import React from 'react';
+const test = () => {
+    return (React.createElement("p", null, "Hola Mundo"));
+};
---
+++ src/test.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const test = () => {
+  return (
+    <p>Hola Mundo</p>
+  )
+}
